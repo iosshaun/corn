2cb9e22b7f55da45026540c1c656bcd162cb6cd8
Shaun Etherton (air) <iosshaun@gmail.com>
2013-11-19 13:32:08 +1030

    added routing, added functions for list and add.

--- multicorn.js
+++ multicorn.js
@@ -14,8 +14,94 @@ console.log('Sending default message to unicorn.')
 eb.send('unicorn', 'shaun' , function(r) {
         console.log("Matched: " + r);
     });
-    
 
+
+var routeMatcher = new vertx.RouteMatcher();
+
+
+routeMatcher.get("/list", function(req) {
+        _handleList(req, true);
+    });    
+
+routeMatcher.get("/add/:word", function(req) {
+        var word = req.params().get("word");
+        word = decodeURI(word);        
+        console.log('word is '+word);
+        _handleAdd(req, word, true);
+    });    
+
+routeMatcher.get("/search/:word", function(req) {
+        var word = req.params().get("word");
+        word = decodeURI(word);        
+        console.log('word is '+word);
+        _handleSearch(req, word, true);
+    });    
+
+routeMatcher.optionsWithRegEx("/search/:word", function(req) {
+        console.log('options route called.');
+        _handleOptions(req, true);        
+    });    
+
+routeMatcher.noMatch(function(req) {
+        req.response.end('Nothing matched');
+    });
+
+
+function _handleList(request,  end){
+    console.log('_handleList called.');
+    eb.send('unicornication', "lists all the items available - potentially huge!" , function(r) {
+            console.log("unicornication service returned: " + r); 
+            var length = (""+r).length;
+            request.response.putHeader("Content-length", length);
+            request.response.putHeader("Access-Control-Allow-Origin", "*"); //xdom
+            request.response.write(""+r+"", "UTF-8");
+            if (end == true)
+                request.response.end();     
+        });     
+}
+
+
+function _handleAdd(request, word, end){
+    console.log('_handleAdd called.');
+    eb.send('unicornholio', word , function(r) {
+            console.log("unicornholio service returned: " + r); 
+            var length = (""+r).length;
+            request.response.putHeader("Content-length", length);
+            request.response.putHeader("Access-Control-Allow-Origin", "*"); //xdom
+            request.response.write(""+r+"", "UTF-8");
+            if (end == true)
+                request.response.end();     
+        });     
+}
+
+function _handleSearch(request, word, end){
+    console.log('_handleSearch called.');
+    eb.send('unicorn', word , function(r) {
+            console.log("unicorn service returned: " + r); 
+            var length = (""+r).length;
+            request.response.putHeader("Content-length", length);
+            request.response.putHeader("Access-Control-Allow-Origin", "*"); //xdom
+            request.response.write(""+r+"", "UTF-8");
+            if (end == true)
+                request.response.end();     
+        });     
+}
+
+
+function _handleOptions(request, end){
+    request.response.putHeader("Access-Control-Allow-Origin",      "*");
+    request.response.putHeader("Access-Control-Allow-Methods",     "POST, GET, PUT, DELETE, OPTIONS");
+    request.response.putHeader("Access-Control-Allow-Credentials", 'false');
+    request.response.putHeader("Access-Control-Max-Age",           '86400'); // 24 hours
+    request.response.putHeader("Access-Control-Allow-Headers",     "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");            
+    if (end == true)
+        request.response.end("ok");    
+}
+
+vertx.createHttpServer().requestHandler(routeMatcher).listen(8080);
+
+
+/*
 vertx.createHttpServer().requestHandler(function(request) {
 
         if (request.method() === 'OPTIONS') {
@@ -36,18 +122,19 @@ vertx.createHttpServer().requestHandler(function(request) {
 
             var word = request.uri();
             word = word.substring(2);
-            word = decodeURI(word);            
-            eb.send('unicorn', word , function(r) {
-                    console.log("WebServer call matched: " + r); 
+            word = decodeURI(word);        
+            eb.send('unicorn', word , function(r) {    
+                    console.log("unicorn service returned: " + r); 
                     var length = (""+r).length;
                     request.response.putHeader("Content-length", length);
                     request.response.putHeader("Access-Control-Allow-Origin", "*"); //xdom
                     request.response.write(""+r+"", "UTF-8");
-                    request.response.end();                    
-                }); 
-                        
+                    if (end == true)
+                        request.response.end();     
+                });
+            
         }
 
     }).listen(8080)
 
-
+*/
--- unicorn.js
+++ unicorn.js
@@ -2,6 +2,23 @@ var vertx = require('vertx');
 var console = require('vertx/console');
 var eb = require("vertx/event_bus");
 
+
+eb.registerHandler('unicornication', function(word, f) {
+        console.log('Received: ' + word);
+        var r = list();
+        f(r);
+    });
+
+
+eb.registerHandler('unicornholio', function(word, f) {
+        console.log('Received: ' + word);
+        // Now reply to it
+        tids[tids.length] = word;
+        build();        
+        f("Added: "+word);
+    });
+
+
 eb.registerHandler('unicorn', function(word, f) {
         console.log('Received: ' + word);
         // Now reply to it
@@ -18,63 +35,76 @@ tids[3] = "shaun ethe";
 tids[4] = "shaun gray";
 tids[5] = "linda";
 tids[6] = "linker";
+var structure;
 
-tids.sort();
-sorted = tids;
-
-var structure = {};
-var n = structure;
-var all = [];
-var allIndex = 0;
-
-for(var i=0; i<sorted.length; i++){
-    var w = tids[i];
-    n = structure;
-    var keyStack = [];    
-    //for chars in word
-    for(var j=0; j<w.length; j++){
-        var c = w.charAt(j);
-        var temp = n[c];
-        if (temp != undefined) {
-            n = temp;
-            keyStack[keyStack.length] = n;
-        }else{
-            n[c]   = {};
-            n[c]['_'] = [];
-            all[allIndex] = n[c];
-            keyStack[keyStack.length] = n[c];
-            n = n[c];
-            allIndex++;            
-        }//end else        
-    }//end for chars in word
-    //backfill keys for word.
-    //console.log('keystack length '+keyStack.length);    
-    function has(a, i){
-        for (var zx=0; zx<a.length; zx++){
-            if (a[zx] == i ){
-                return true;
-            }
-            return false;
-        }
+function list(){
+    tids.sort();
+    var sorted = tids;
+    var r = "";
+    for(var i=0; i<sorted.length; i++){
+       r += tids[i]+"\n";
     }
+    return r;
+}
+
+function build(){
+    structure = {};
+    tids.sort();
+    var sorted = tids;
     
-    for (var z=0; z<keyStack.length; z++){
-        var x = keyStack[z];
-        var a = x._;
-        if (! has(a, i) ){
-            a.push(i);
-        }        
-    }    
-}//end for words in list
+    var n = structure;
+    var all = [];
+    var allIndex = 0;
+    
+    for(var i=0; i<sorted.length; i++){
+        var w = tids[i];
+        n = structure;
+        var keyStack = [];    
+        //for chars in word
+        for(var j=0; j<w.length; j++){
+            var c = w.charAt(j);
+            var temp = n[c];
+            if (temp != undefined) {
+                n = temp;
+                keyStack[keyStack.length] = n;
+            }else{
+                n[c]   = {};
+                n[c]['_'] = [];
+                all[allIndex] = n[c];
+                keyStack[keyStack.length] = n[c];
+                n = n[c];
+                allIndex++;            
+            }//end else        
+        }//end for chars in word
+        //backfill keys for word.
+        //console.log('keystack length '+keyStack.length);    
+        function has(a, i){
+            for (var zx=0; zx<a.length; zx++){
+                if (a[zx] == i ){
+                    return true;
+                }
+                return false;
+            }
+        }
+        
+        for (var z=0; z<keyStack.length; z++){
+            var x = keyStack[z];
+            var a = x._;
+            if (! has(a, i) ){
+                a.push(i);
+            }        
+        }    
+    }//end for words in list
 
-
-//search function to match a word in a structure
+}//end build
+    
+    //search function to match a word in a structure
 function findMatches(structure, word){
     var node = structure;
     var _matches = [];
     for (var i = 0; i < word.length; i++) {
         var c = word.charAt(i);
-        if (node[c] == null) {
+        if (node[c] == null || node[c] == undefined) {
             break;
         } else {
             _matches = node[c]._;
@@ -83,11 +113,16 @@ function findMatches(structure, word){
     }
     var matches = [] ;
     for (var j = 0; j < _matches.length; j++) {
-        console.log('matched: ' + tids[_matches[j]]);
-        matches[j] = tids[_matches[j]];
+        var m = tids[_matches[j]];
+        console.log('matched: ' + m);
+        if (m.length >= word.length)
+            matches[j] = m;
     }
     return matches;
 }
+
+//build a structure.
+build();
 
 //quick test.
 findMatches(structure, 'uni');
